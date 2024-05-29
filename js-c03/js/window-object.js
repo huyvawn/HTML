f640e887a24d3219af41c957c316ba79fb3f7449
huyvawn <63355258+huyvawn@users.noreply.github.com>
2024-05-29 09:24:56 +0700

    update js-c03

---
+++ js-c03/js/window-object.js
@@ -0,0 +1,7 @@
+var msg = '<h2>browser window</h2><p>width: ' + window.innerWidth + '</p>';
+
+msg+= '<p>height ' +window.innerHeight + '</p>';
+
+msg += '<h2>history</h2><p>items: ' +window.history.length + '</p>';
+
+msg += 
